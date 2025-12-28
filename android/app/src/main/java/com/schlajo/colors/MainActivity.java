e4fcb2a0f62720ee1765fea51e8020feaa3b403d
schlajo <schlajo1981@gmail.com>
2025-12-28 17:40:10 -0600

    Made splash page better.  Made app responsive to increased font to be more WCAG.

--- android/app/src/main/java/com/schlajo/colors/MainActivity.java
+++ android/app/src/main/java/com/schlajo/colors/MainActivity.java
@@ -1,5 +1,16 @@
 package com.schlajo.colors;
 
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        
+        // Enable text zooming to respect system font size settings
+        WebSettings webSettings = this.bridge.getWebView().getSettings();
+        webSettings.setTextZoom(100); // Use system default (100%)
+        webSettings.setSupportZoom(true);
+    }
+}
